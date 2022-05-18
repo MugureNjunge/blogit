82f52d4de2dd0c406bbd77fca7521d04299b2a3d
MugureNjunge <maureennjunge22@gmail.com>
2022-05-18 20:02:52 +0300

    add requests for the random quote generator

---
+++ app/request.py
@@ -0,0 +1,21 @@
+from app import app
+import urllib.request,json
+from .quote import Quotes
+
+
+def get_quotes():
+    with urllib.request.urlopen('http://quotes.stormconsultancy.co.uk/random.json') as url:
+        data = url.read()
+        get_json = json.loads(data)
+
+        if get_json['author'] and get_json['quote']:
+
+            author = get_json['author']
+            quote = get_json['quote']
+
+            new_quote = Quotes(author, quote)
+
+            return new_quote
+
+        else:
+            return None
